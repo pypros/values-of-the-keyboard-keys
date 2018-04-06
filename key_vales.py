fb9e8f381fa89c347aba122e7ce9de486bc1f676
Lukasz Sobczuk <sobczuk.lukasz@wp.pl>
2018-04-06 09:39:11 +0200

    Create key_vales.py

---
+++ key_vales.py
@@ -0,0 +1,23 @@
+import sys
+import tty
+
+def get_key_value():
+    DATA_BUFFER_BUFFER = 3
+    fd = sys.stdin.fileno()
+    tty.setcbreak(fd)
+    key = sys.stdin.read(DATA_BUFFER_BUFFER)
+    return key
+
+def get():
+    while True:
+        key_value = get_key_value()
+        if key_value:
+            break
+    return key_value.encode()
+
+def main():
+    for i in range(0,4):
+        print(get())
+
+if __name__=='__main__':
+        main()
